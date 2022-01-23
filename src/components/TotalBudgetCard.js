a4afb907f4aa0189de98a8cfd0909813ec30bd3c
SpyrosGeo <xarodasimba@gmail.com>
2022-01-24 01:44:21 +0200

    fixed a bug with totalbudgetcard and added functionality to move deleted expenses to uncategorized if the bugdet is deleted

--- src/components/TotalBudgetCard.js
+++ src/components/TotalBudgetCard.js
@@ -3,10 +3,10 @@ import BudgetCard from './BudgetCard'
 import {useBudgets} from '../contexts/BudgetContexts'
 
 
-export default function TotalBudgetCard(props) {
+export default function TotalBudgetCard() {
     const {expenses,budgets} = useBudgets()
     const amount = expenses.reduce((total,expense)=>total+expense.amount,0)
-    const max = budgets.reduce((total,budget)=> total+budget.max)
+    const max = budgets.reduce((total,budget)=> total+budget.max,0)
     if (max===0)return null
     return (
         <BudgetCard amount={amount}  gray name="Total" max={max} hideButtons />
